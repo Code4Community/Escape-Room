7e9782e60d70f7cf3ccca4ad88758207c365704c
Erol Sonmez <erolsonmeznj@gmail.com>
2024-09-30 19:50:13 -0400

    Deleted files in main

--- src/components/enemy-components.js
+++
@@ -1,5 +0,0 @@
-import { defineComponent, Types } from "bitecs";
-
-const Enemy = defineComponent();
-
-export { Enemy };
--- src/components/star-components.js
+++
@@ -1,5 +0,0 @@
-import { defineComponent, Types } from "bitecs";
-
-const Star = defineComponent();
-
-export { Star };
